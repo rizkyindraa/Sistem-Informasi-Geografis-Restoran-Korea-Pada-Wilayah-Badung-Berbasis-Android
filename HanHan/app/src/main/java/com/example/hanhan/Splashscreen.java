3f33a32a53f1e9937405f970670f9825e292f984
rizkyindraa <rizkyindrawibowo99@gmail.com>
2020-06-09 09:25:38 +0800

    folder project android
    pengumpulan project uas SIG

---
+++ HanHan/app/src/main/java/com/example/hanhan/Splashscreen.java
@@ -0,0 +1,28 @@
+package com.example.hanhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splashscreen extends AppCompatActivity {
+
+    private final int panjang = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent tampil = new Intent(Splashscreen.this,MainActivity.class);
+                Splashscreen.this.startActivity(tampil);
+                Splashscreen.this.finish();
+            }
+        }, panjang);
+
+    }
+}
